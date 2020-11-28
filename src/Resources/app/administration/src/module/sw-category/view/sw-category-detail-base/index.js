9bb54fff1017fb77e85945bb69f5f3e158a74ffb
juanitolaguna <little.pilgrim@mail.ru>
2020-11-28 23:50:59 +0100

    add image to link category in admin

---
+++ src/Resources/app/administration/src/module/sw-category/view/sw-category-detail-base/index.js
@@ -0,0 +1,6 @@
+import template from './sw-category-detail-base.html.twig'
+const {Component} = Shopware;
+
+Component.override('sw-category-detail-base', {
+    template,
+});
---
+++ src/Subscriber/StorefrontSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace EventCandyUtils\Subscriber;
+
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Shopware\Storefront\Event\StorefrontRenderEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class StorefrontSubscriber implements EventSubscriberInterface
+{
+
+    /**
+     * @var SystemConfigService
+     */
+    private $systemConfigService;
+
+    public function __construct(SystemConfigService $systemConfigService)
+    {
+        $this->systemConfigService = $systemConfigService;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            StorefrontRenderEvent::class => 'onStorefrontRender',
+        ];
+    }
+
+    /**
+     * @param StorefrontRenderEvent $event
+     */
+    public function onStorefrontRender(StorefrontRenderEvent $event)
+    {
+        $config = $this->systemConfigService->get('EventCandyUtils.config');
+        $event->setParameter('ecUtilsConfig', $config);
+    }
+
+}
