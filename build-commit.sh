37cc66905637c864485ade296ecac592eca6925c
juanitolaguna <little.pilgrim@mail.ru>
2021-01-08 01:37:06 +0100

    add build script

---
+++ build-commit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+pluginName="EventCandyUtils"
+
+#docker exec -it shopware /var/www/html/bin/build-js.sh
+
+copy new storefront build
+sudo rm -rf ./src/Resources/app/storefront/dist/storefront
+docker cp shopware:/var/www/html/custom/plugins/$pluginName/src/Resources/app/storefront/dist/. \
+./src/Resources/app/storefront/dist
+
+#copy new administration
+sudo rm -rf ./src/Resources/public/administration
+docker cp shopware:/var/www/html/custom/plugins/$pluginName/src/Resources/public/. \
+./src/Resources/public
+
+
+timestamp=$(date +%m-%d-%Y-%s)
+git add .
+git commit -m "build-${timestamp}"
+git push origin main
